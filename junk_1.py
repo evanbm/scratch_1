53c56981c2d9ef7d4576d6e1ce5924020d477965
Evan Bullock-Morales <evan.bm@gmail.com>
2017-11-05 17:25:56 -0800

    Some changes

--- junk_1.py
+++ junk_1.py
@@ -1,5 +1,5 @@
 """
-Junk 
+    Junk 
 """
 
 import sqlite3
@@ -10,19 +10,19 @@ STRING = "thiS Is an iSsue involving IsiS"
 
 
 class MySum(object):
+    '''
+        My Sum
+    '''
     def __init__(self):
         self.count = 0
         self.con = sqlite3.connect(":memory:")
         self.con.create_aggregate("mysum", 1, MySum)
         self.cur = self.con.cursor()
 
-    def step(self, value):
-        self.count += value
-
-    def finalize(self):
-        return self.count
-
     def my_method(self):
+        '''
+            My method
+        '''
         self.cur.execute("CREATE TABLE test(i)")
         self.cur.execute("INSERT INTO test(i) VALUES (1)")
         self.cur.execute("INSERT INTO test(i) VALUES (2)")
@@ -30,10 +30,28 @@ class MySum(object):
         self.con.commit()
 
     def fetch_one(self):
+        '''
+            fetch one.
+        '''
         return self.cur.fetchone()[0]
 
+    def step(self, value):
+        '''
+            step
+        '''
+        self.count += value
+
+    def finalize(self):
+        '''
+            finalize
+        '''
+        return self.count
 
 def main():
+    '''
+        main
+    '''
+
     ms = MySum()
 
     for match in re.finditer("[iI][sS]", STRING):
